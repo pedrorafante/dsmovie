f4969a91da3a2c1f67c4459031620e85b2922cf3
Pedro Rafante <pedrorafante@gmail.com>
2022-01-16 09:59:03 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -4,17 +4,17 @@ import { Movie } from 'types/movie';
 import './styles.css';
 
 type Props = {
-    movie : Movie;
+    movie: Movie;
 }
 
-function MovieCard({ movie } : Props) {
+function MovieCard({ movie }: Props) {
 
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore count={movie.count} score={movie.score} />
 
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
